f0f4c0aa5ea90a7106d623a10260b0ee6d7b7454
lockykeaney <lockykeaney@gmail.com>
2017-08-15 19:27:58 +1000

    added icons to PostVotes

--- src/components/PostVotes.js
+++ src/components/PostVotes.js
@@ -1,19 +1,38 @@
 import React from 'react';
 import { css } from 'glamor';
+import { Icon } from 'react-fa';
 
 let voteStyle = css({
   width: '20%',
   textAlign: 'center',
   fontSize: '28px',
-  color: 'white'
+  color: 'white',
+  display: 'flex',
+  alignItems: 'center',
+  flexDirection: 'column',
 })
+let arrow = css({
+  cursor: 'pointer',
+  color: 'white',
+  ':hover': {
+    color: 'red'
+  }
+})
+
 
 const PostVotes = ({ votes }) =>
 
     <div {...voteStyle}>
-      <div></div>
+      <Icon
+        {...arrow}
+        name='arrow-up'
+        size='1x'
+      />
       {votes}
-      <div></div>
+      <Icon
+        {...arrow}
+        name='arrow-down'
+      />
     </div>
 
 export default PostVotes;
